b39756059bff818de2c0542469238e637d583ebe
eitak <eitak@users.noreply.github.com>
2016-05-30 21:16:03 +0100

    Add REMOVE_FROM_CART in shopping-cart example; Update to use v0.0.3 redux-live in examples

--- examples/shopping-cart/src/shared/reducers/cart.js
+++ examples/shopping-cart/src/shared/reducers/cart.js
@@ -1,5 +1,7 @@
+import _ from 'lodash'
+
 import {SET_STREAM_INITIAL_STATE} from 'redux-live/lib/shared/constants/ActionTypes'
-import {ADD_TO_CART} from '../../shared/constants/ActionTypes'
+import {ADD_TO_CART, REMOVE_FROM_CART} from '../../shared/constants/ActionTypes'
 
 export default function cart(state = {addedProducts: {}}, action) {
     switch (action.type) {
@@ -16,6 +18,14 @@ export default function cart(state = {addedProducts: {}}, action) {
                 ...state,
                 addedProducts: Object.assign({}, state.addedProducts, {[productId]: {quantity: newCount}})
             };
+        case REMOVE_FROM_CART:
+            const productId = action.productId;
+            const newCount = Math.max((state.addedProducts[productId] || {quantity: 0}).quantity - 1, 0);
+            if (newCount === 0) {
+                return {...state, addedProducts: _.omit(state.addedProducts, productId)}
+            } else {
+                return {...state, addedProducts: Object.assign({}, state.addedProducts, {[productId]: {quantity: newCount}})}
+            }
         default:
             return state
     }
